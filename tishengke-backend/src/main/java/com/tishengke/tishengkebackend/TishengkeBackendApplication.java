6184bbb2fd0860e59282baaec6a215c75097708c
Yes-I-Du <dumz999@163.com>
2025-06-04 23:43:02 +0800

    修复子模块问题

---
+++ tishengke-backend/src/main/java/com/tishengke/tishengkebackend/TishengkeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tishengke.tishengkebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TishengkeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TishengkeBackendApplication.class, args);
+    }
+
+}
---
+++ tishengke-backend/src/test/java/com/tishengke/tishengkebackend/TishengkeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tishengke.tishengkebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TishengkeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
